c0cb75e1f94a04c48749fedaff7f5ead9e74117f
Nate Kim <glwithu06@gmail.com>
2019-01-22 18:11:57 +0900

    Implement semantic version

---
+++ Semver/Semver.h
@@ -0,0 +1,19 @@
+//
+//  Semver.h
+//  Semver
+//
+//  Created by Nate Kim on 22/01/2019.
+//  Copyright © 2019 glwithu06. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Semver.
+FOUNDATION_EXPORT double SemverVersionNumber;
+
+//! Project version string for Semver.
+FOUNDATION_EXPORT const unsigned char SemverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Semver/PublicHeader.h>
+
+
