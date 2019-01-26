cafd574f5b54c5e7e389423b592b549115007e63
Nate Kim <glwithu06@gmail.com>
2019-01-26 18:28:02 +0900

    Delete project files

--- Package.swift
+++ Package.swift
@@ -1,24 +1,21 @@
 // swift-tools-version:4.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Semver",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Semver",
             targets: ["Semver"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Semver",
-            dependencies: []),
+            path: "Sources"),
         .testTarget(
             name: "SemverTests",
-            dependencies: ["Semver"]),
+            dependencies: ["Semver"],
+            path: "Tests"),
     ]
 )
