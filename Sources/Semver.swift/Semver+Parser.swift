5b2b5e02ded68fe129bae1d2d739493f381db3b3
Nate Kim <glwithu06@gmail.com>
2019-01-26 18:11:58 +0900

    Add Package.swift for SPM

---
+++ Sources/Semver.swift/Semver+Parser.swift
@@ -0,0 +1,151 @@
+//
+//  Semver+Parser.swift
+//  Semver
+//
+//  Created by Nate Kim on 21/01/2019.
+//  Copyright © 2019 glwithu06. All rights reserved.
+//
+
+import Foundation
+
+internal let dotDelimeter = "."
+internal let prereleaseDelimeter = "-"
+internal let buildMetaDataDelimeter = "+"
+
+extension Semver {
+    public enum ParsingError: Error {
+        public enum Component {
+            case major, minor, patch, prereleaseIdentifiers, buildMetadataIdentifiers
+        }
+        case malformedString(String, Error?)
+        case digitsNotFound(String)
+        case parseError(Component, Error)
+
+        var localizedDescription: String {
+            switch self {
+            case .malformedString(let version, let e):
+                return "malformed \"\(version)\"\t\(e.map { "\nInfo)" + $0.localizedDescription } ?? "")"
+            case .digitsNotFound(let version):
+                return "There are no digits in \(version)"
+            case .parseError(let component, let e):
+                return "\(component) error: \(e.localizedDescription)"
+            }
+        }
+    }
+
+    static public func parse(_ input: String) throws -> Semver {
+        let major: String
+        let minor: String
+        let patch: String
+        let prereleaseIdentifiers: [String]
+        let buildMetadataIdentifiers: [String]
+        let extra: String
+
+        let validateRegex = try NSRegularExpression(pattern: "^([0-9A-Za-z\(prereleaseDelimeter)\\\(dotDelimeter)|\\\(buildMetaDataDelimeter)]+)$")
+        guard input.rangeOfFirstMatch(for: validateRegex).length == input.count else {
+            throw ParsingError.malformedString(input, nil)
+        }
+
+        let scanner = Scanner(string: input)
+        let prefix = scanner.scanUpToCharacters(from: CharacterSet.decimalDigits)
+        guard prefix != "-" else { throw ParsingError.malformedString(input, nil) }
+
+        let delimeterCharacterSet = CharacterSet(charactersIn: "\(prereleaseDelimeter)\(buildMetaDataDelimeter)")
+        guard let versionStr = scanner.scanUpToCharacters(from: delimeterCharacterSet) else {
+            throw ParsingError.malformedString(input, nil)
+        }
+
+        let versionScanner = Scanner(string: versionStr)
+        guard let majorStr = versionScanner.scanCharacters(from: CharacterSet.decimalDigits) else {
+            throw ParsingError.digitsNotFound(input)
+        }
+        major = majorStr
+
+        func scanNextVersion(_ scanner: Scanner) throws -> String {
+            guard !scanner.isAtEnd else {
+                return "0"
+            }
+            scanner.scanString(dotDelimeter, into: nil)
+            guard let digits = scanner.scanCharacters(from: CharacterSet.decimalDigits) else {
+                throw ParsingError.digitsNotFound(scanner.string)
+            }
+            return digits
+        }
+        minor = try scanNextVersion(versionScanner)
+        patch = try scanNextVersion(versionScanner)
+
+        let scanIndex = String.Index(encodedOffset: scanner.scanLocation)
+        extra = String(input[scanIndex...])
+        do {
+            let prereleaseRegex = try NSRegularExpression(pattern: "(?<=\(prereleaseDelimeter))([0-9A-Za-z-\\.]+)")
+            prereleaseIdentifiers = Range(extra.rangeOfFirstMatch(for: prereleaseRegex), in: extra)
+                .map { String(extra[$0]) }
+                .map { $0.components(separatedBy: dotDelimeter) }
+                ?? []
+        } catch let e {
+            throw ParsingError.parseError(.prereleaseIdentifiers, e)
+        }
+
+        do {
+            let buildMetadataRegex = try NSRegularExpression(pattern: "(?<=\\\(buildMetaDataDelimeter))([0-9A-Za-z-\\.]+)")
+            buildMetadataIdentifiers = Range(extra.rangeOfFirstMatch(for: buildMetadataRegex), in: extra)
+                .map { String(extra[$0]) }
+                .map { $0.components(separatedBy: dotDelimeter) }
+                ?? []
+        } catch let e {
+            throw ParsingError.parseError(.buildMetadataIdentifiers, e)
+        }
+
+        let prerelease = prereleaseIdentifiers.count > 0 ? prereleaseDelimeter + prereleaseIdentifiers.joined(separator: dotDelimeter) : ""
+        let metadata = buildMetadataIdentifiers.count > 0 ? buildMetaDataDelimeter + buildMetadataIdentifiers.joined(separator: dotDelimeter) : ""
+        guard extra.replacingOccurrences(of: prerelease, with: "").replacingOccurrences(of: metadata, with: "").count == 0 else {
+            throw ParsingError.malformedString(input, nil)
+        }
+
+        return Semver(major: major,
+                      minor: minor,
+                      patch: patch,
+                      prereleaseIdentifiers: prereleaseIdentifiers,
+                      buildMetadataIdentifiers: buildMetadataIdentifiers)
+    }
+}
+
+extension String {
+    public var version: Semver? {
+        return try? Semver.parse(self)
+    }
+}
+
+extension Numeric {
+    public var version: Semver? {
+        return try? Semver.parse("\(self)")
+    }
+}
+
+extension String {
+
+    fileprivate func rangeOfFirstMatch(for regex: NSRegularExpression) -> NSRange {
+        return regex.rangeOfFirstMatch(in: self, range: NSRange(location: 0, length: self.count))
+    }
+
+    internal func firstMatch(for regex: NSRegularExpression) -> String? {
+        return regex.firstMatch(in: self, range: NSRange(location: 0, length: self.count))
+            .map { Range($0.range, in: self)! }
+            .map { String(self[$0]) }
+    }
+}
+
+extension Scanner {
+
+    fileprivate func scanUpToCharacters(from: CharacterSet) -> String? {
+        var str: NSString?
+        scanUpToCharacters(from: from, into: &str)
+        return str as String?
+    }
+
+    fileprivate func scanCharacters(from: CharacterSet) -> String? {
+        var str: NSString?
+        scanCharacters(from: from, into: &str)
+        return str as String?
+    }
+}
---
+++ Tests/SemverTests/SemverTests.swift
@@ -0,0 +1,70 @@
+//
+//  SemverTests.swift
+//  SemverTests
+//
+//  Created by Nate Kim on 22/01/2019.
+//  Copyright © 2019 glwithu06. All rights reserved.
+//
+
+import XCTest
+@testable import Semver
+
+class SemverTests: XCTestCase {
+
+    func testEqualBasicVersion() {
+        XCTAssertEqual("1.100.3".version!, Semver(major: "1", minor: "100", patch: "3"))
+        XCTAssertNotEqual("1.100.3".version!, "1.101.3".version!)
+        // Big number
+        XCTAssertEqual("69938113471411635120691317071569414.452.368".version!,
+                       Semver(major: "69938113471411635120691317071569414", minor: "452", patch: "368"))
+        XCTAssertNotEqual("69938113471411635120691317071569414.452.36".version!,
+                          "69938113471411635120691317071569414.452.368".version!)
+        // prefixed
+        XCTAssertEqual("semver.1.100.3".version!, Semver(major: "1", minor: "100", patch: "3"))
+        XCTAssertNotEqual("semver.1.100.3".version!, "semver.2.100.3".version!)
+    }
+
+    func testEqualPrefixedVersion() {
+        XCTAssertEqual("semver.1.100.3".version!, "v1.100.3".version!)
+        XCTAssertEqual("semver.1.100.3-rc.1".version!, "ver1.100.3-rc.1".version!)
+    }
+
+    func testEqualPrereleaseVersion() {
+        XCTAssertEqual("1.100.3-rc.1".version!, Semver(major: "1", minor: "100", patch: "3", prereleaseIdentifiers: ["rc", "1"]))
+        XCTAssertNotEqual("1.100.3-rc.1".version!, "1.101.3-rc.2".version!)
+        XCTAssertNotEqual("1.100.3-alpha".version!, "1.101.3-beta".version!)
+        XCTAssertNotEqual("1.100.3-rc.a".version!, "1.101.3-rc.1".version!)
+    }
+
+    func testEqualBuildmetadataVersion() {
+        XCTAssertEqual("1.101.345+build.sha.111".version!, Semver(major: "1", minor: "101", patch: "345"))
+        XCTAssertEqual("1.101.345+build.sha.111".version!, "1.101.345+test.metadata".version!)
+        XCTAssertEqual("1.101.345-rc.1+build.sha.111".version!, "1.101.345-rc.1+test.metadata".version!)
+        XCTAssertNotEqual("1.101.345-rc.1+build.sha.111".version!, "1.101.345-rc.2+build.sha.111".version!)
+    }
+
+    func testCompareBasicVersion() {
+        // Compare Major
+        XCTAssertLessThan("1.100.0".version!, "2.0.0".version!)
+        // Compare Minor
+        XCTAssertLessThan("1.99.231".version!, "1.101.12344".version!)
+        // Compare Patch
+        XCTAssertLessThan("1.99.231".version!, "1.99.12344".version!)
+    }
+
+    func testCompareBasicAndPrereleaseVersion() {
+        XCTAssertLessThan("1.99.231-alpha".version!, "1.99.231".version!)
+    }
+
+    func testComparePrereleaseVersion() {
+        // alphabetical order
+        XCTAssertLessThan("1.99.231-test.alpha".version!, "1.99.231-test.beta".version!)
+        XCTAssertLessThan("1.99.231-test.19b".version!, "1.99.231-test.alpha".version!)
+
+        // numeric < non-numeric
+        XCTAssertLessThan("1.99.231-test.2".version!, "1.99.231-test.19b".version!)
+
+        // smaller-set < larger-set
+        XCTAssertLessThan("1.99.231-alpha.beta".version!, "1.99.231-alpha.beta.11".version!)
+    }
+}
